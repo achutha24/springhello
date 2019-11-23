699bee211dd2d57d351ebc98dbc2ef4526821c7a
Achutha <achutha.ya@gmail.com>
2019-11-23 19:00:43 +0530

    Added swagger config

--- src/main/java/com/hello/demo/controller/EmployeeController.java
+++ src/main/java/com/hello/demo/controller/EmployeeController.java
@@ -1,13 +1,19 @@
 package com.hello.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Api(value = "Demo APIs", tags = "Demo APIs Spring Boot")
 public class EmployeeController {
 	
-	@RequestMapping("/welcome")
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	@ApiOperation(value = "To Get Welcome Message")
     public String getMessage() 
     {
 		return "Hello, Welcome to Spring Boot!";
